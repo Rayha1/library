d5250b98ae997fc65465a2ca916d7b0270634eb5
Rayha1 <103225542+Rayha1@users.noreply.github.com>
2023-05-07 18:28:24 +1200

    error checked for entering duplicate books

--- GUI.java
+++ GUI.java
@@ -5,70 +5,103 @@ import ecs100.*;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class GUI
-{
-    // instance variables - replace the example below with your own
-    private Books books;
-    private Book book;
-    private int quantity = 0;
-    private int currBook = 1;
 
-    /**
-     * Constructor for objects of class GUI
-     */
-    public GUI()
-    {
-        // initialise instance variables
-        books = new Books();
-        UI.initialise();
-        UI.addButton("ALL", this::allBooks);
-        UI.addButton("Add", this::addBook);
-        UI.addButton("Find", this::findBook);
-        UI.addButton("Quit", UI::quit);
+public class GUI {
+  // instance variables - replace the example below with your own
+  private Books books;
+  private Book book;
+  private int quantity = 0;
+  private int currBook = 1;
+  
+  /**
+  * Constructor for objects of class GUI.
+  */
+   
+  public GUI() {
+    // initialise instance variables
+    books = new Books();
+    UI.initialise();
+    UI.addButton("ALL", this::allBooks);
+    UI.addButton("Add", this::addBook);
+    UI.addButton("Find", this::findBook);
+    UI.addButton("Quit", UI::quit);
         
-        UI.setMouseListener(this::doMouse);
-    }
+    UI.setMouseListener(this::doMouse);
+  }
 
-    /**
-     * adds a book
-     */
-    public void addBook() {
-        final int MAX_QUANTITY = 999;
-        final int MAX_PAGES = 21450; // one piece volumes put together
-        int pages;
-        int quantity = 0;
-        //ask user for details
-        String name = UI.askString("Title: ");
-        String author = UI.askString("Author: ");
-        while (quantity == 0) {
-            pages = UI.askInt("Quantity: "); // requires validation 
-            if (pages < MAX_PAGES || pages > 0) {
-                quantity = pages;
+  /**
+  * adds a book.
+  */
+  public void addBook() {
+    final int MAX_QUANTITY = 999; // max quantity of books
+    final int MAX_PAGES = 21450; // one piece volumes put together
+    int pages; // only gets value if quantitiy value is approrpriate
+    String sameTitle; // checks if titel is same
+    String sameAuthor; // checks if author is same
+    String name = "blue"; // name of book
+    String author = "bob"; //author of book
+    Boolean check = true; // loop to check if book exists
+    Boolean same = false; // true if the same book exists
+    int quantity = 0;
+    //ask user for details
+    // checks if the same book already exits
+    while (check == true) {
+        name = UI.askString("Title: ");
+        author = UI.askString("Author: ");
+        for (int bookId : books.returnList().keySet()) {
+            Book book = books.returnList().get(bookId); // sets current book
+            sameTitle = books.findTitle(bookId); // gets curr title
+            sameAuthor = book.getAuthor(); // gets curr author
+            if ((sameTitle.equals(name)) && (sameAuthor.equals(author))) {
+                //if book matches
+                same = true;
             }
         }
-        String imgFileName = UIFileChooser.open("Choose Image File: ");
-        books.addBook(name, author, quantity, imgFileName);
-    }
-    
-    /**
-     * finds a book based on name
-     * prints out author, quantity, book cover
-     */
-    public void findBook() {
-        UI.clearGraphics();
-        String bookName = UI.askString("Name of book: ");
-        if (books.findBook(bookName)) {
-            UI.println("Found Book!");
-            book = books.getBook();
-            UI.println("Author: " + book.getAuthor());
-            UI.println("Quantity: " + book.getQuantity());
-            book.displayBook(currBook); // Show books cover
+        // if no duplicate book was found
+        if (same == false){
+            while (quantity == 0) {
+              pages = UI.askInt("Quantity: "); // requires validation 
+              if (pages < MAX_PAGES && pages > 0) {
+                quantity = pages;
+              }
+            }
+            String imgFileName = UIFileChooser.open("Choose Image File: ");
+            books.addBook(name, author, quantity, imgFileName);
+            check = false;
         } else {
-            UI.println("Book not found");
+            // if book exists
+            UI.println("book already exists");
+            UI.println("to try again");
+            UI.println("press 'Add' button");
+            check = false;
         }
     }
+
+        
+  }
     
-    public void allBooks() {
+  /**
+  * finds a book based on name
+  * prints out author, quantity, book cover.
+  */
+  public void findBook() {
+    UI.clearGraphics();
+    String bookName = UI.askString("Name of book: ");
+    if (books.findBook(bookName)) {
+      UI.println("Found Book!");
+      book = books.getBook();
+      UI.println("Author: " + book.getAuthor());
+      UI.println("Quantity: " + book.getQuantity());
+      book.displayBook(currBook); // Show books cover
+    } else {
+      UI.println("Book not found");
+    }
+  }
+  
+  /**
+   * prints all the book covers in the GUI.
+   */
+  public void allBooks() {
         UI.clearGraphics();
         int size = books.hashLen() + 1;
         String name;
@@ -87,7 +120,7 @@ public class GUI
     } 
 
     /**
-      * select object based on where the usr clicks
+      * select object based on where the usr clicks.
       */
     private void doMouse(String action, double x, double y) {
         if (action.equals("clicked")) {
@@ -101,6 +134,7 @@ public class GUI
             double bottom = book.getBottom();
             if ((x >= left) && (x <= right) && (y >= top) && (y <= bottom)) {
                 UI.println(title + " Borrowed");
+                book.borrowed();
             }
             }
         }
